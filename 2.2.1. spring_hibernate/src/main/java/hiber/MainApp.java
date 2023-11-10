7c3b18b0463f97fba9ce3947d9d5e8c73e165f5f
Artem2605 <artyom260599@mail.ru>
2023-11-10 03:46:00 +0300

    firstSend

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,36 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
-
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
-
-      context.close();
-   }
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        User firstUser = new User("Artem", "Alav", "aaa@mail.ru");
+        Car firstCar = new Car("MERS", 221);
+        firstUser.setCar(firstCar);
+        userService.add(firstUser);
+        User secondUser = new User("Ivan", "Romanov", "sss@mail.ru");
+        Car secondCar = new Car("BMW", 535);
+        secondUser.setCar(secondCar);
+        userService.add(secondUser);
+        User thirdUser = new User("Vasiliy", "Petrov", "qqq@mail.ru");
+        Car thirdCar = new Car("Toyota", 350);
+        thirdUser.setCar(thirdCar);
+        userService.add(thirdUser);
+        List<User> users = userService.listUsers();
+        for (User temp : users) {
+            System.out.println("\n" + temp + "\n");
+        }
+        System.out.println("\n" + userService.getUser(firstCar).toString());
+        context.close();
+    }
 }
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,50 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "MODEL")
+    private String model;
+
+    @Column(name = "SERIES")
+    private Integer series;
+
+    public Car() {
+    }
+
+    public Car(String model, Integer series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getSeries() {
+        return series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(Integer series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}

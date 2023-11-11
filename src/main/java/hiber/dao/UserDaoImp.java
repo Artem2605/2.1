831e613bbd8873f5fed10c055be9ea144d7cb9b3
Artem2605 <artyom260599@mail.ru>
2023-11-11 12:38:38 +0300

    thirdSend

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -5,8 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -15,32 +13,51 @@ public class UserDaoImp implements UserDao {
     @Autowired
     private SessionFactory sessionFactory;
 
-    private TypedQuery<User> listUsersQuery;
+    private List<User> listUsers;
 
     @Override
     public void add(User user) {
-        sessionFactory.getCurrentSession().save(user);
+        try (Session session = sessionFactory.openSession()) {
+            session.save(user);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> getListUsers() {
-        listUsersQuery = sessionFactory.getCurrentSession().createQuery("from User");
-        return listUsersQuery.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            listUsers = session.createQuery("from User").getResultList();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return listUsers;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public User getUserByCar(String model, Integer series) {
-        listUsersQuery = sessionFactory.getCurrentSession().createQuery("from User user " +
-                "where user.car.model = :model and user.car.series = :series").
-                setParameter("model", model).setParameter("series", series);
-        return listUsersQuery.getResultList().get(0);
+        try (Session session = sessionFactory.openSession()) {
+            listUsers = session.createQuery("from User user " +
+                    "where user.car.model = :model and user.car.series = :series").
+                    setParameter("model", model).setParameter("series", series).
+                    getResultList();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return listUsers.get(0);
     }
 
     @Override
     public void removeUserById(Long id) {
-        sessionFactory.getCurrentSession().createQuery("delete User where id = :id").
-                setParameter("id", id).executeUpdate();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete User where id = :id").
+                    setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
